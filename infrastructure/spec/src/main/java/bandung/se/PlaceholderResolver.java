1bd0149ff83dc6efc415100c481075c619db0322
iMinusMinus <mean.leung@outlook.com>
2025-08-02 11:05:45 +0800

    mp-config implementation: indexed property, fix placeholder

--- infrastructure/spec/src/main/java/bandung/se/PlaceholderResolver.java
+++ infrastructure/spec/src/main/java/bandung/se/PlaceholderResolver.java
@@ -1,26 +1,11 @@
 package bandung.se;
 
-public abstract class PlaceholderResolver<CONTEXT> {
+@FunctionalInterface
+public interface PlaceholderResolver<CONTEXT> {
 
-    public static final String DEFAULT_PLACEHOLDER_PREFIX = "${";
-    public static final String DEFAULT_PLACEHOLDER_SUFFIX = "}";
-    public static final String DEFAULT_VALUE_SEPARATOR = ":";
+    String DEFAULT_PLACEHOLDER_PREFIX = "${";
+    String DEFAULT_PLACEHOLDER_SUFFIX = "}";
+    String DEFAULT_VALUE_SEPARATOR = ":";
 
-    protected final String placeHolderPrefix;
-
-    protected final String placeHolderSuffix;
-
-    protected final String valueSeparator;
-
-    public PlaceholderResolver() {
-        this(DEFAULT_PLACEHOLDER_PREFIX, DEFAULT_PLACEHOLDER_SUFFIX, DEFAULT_VALUE_SEPARATOR);
-    }
-
-    public PlaceholderResolver(String placeHolderPrefix, String placeHolderSuffix, String valueSeparator) {
-        this.placeHolderPrefix = placeHolderPrefix;
-        this.placeHolderSuffix = placeHolderSuffix;
-        this.valueSeparator = valueSeparator;
-    }
-
-    public abstract String resolve(String value, CONTEXT context);
+    String resolve(String value, CONTEXT context);
 }
